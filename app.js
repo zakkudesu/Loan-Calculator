4aafd25461191d147ddfc6eb821218a06dd4ae35
Zach Alcerro <zachalcerro@gmail.com>
2019-09-21 13:33:38 +0900

    completes js with loader gif

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 //Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  //Hide Results
+  document.getElementById('results').style.display = 'none';
+
+  //Show Loading
+  e.preventDefault();
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+});
 
 //Calculate Results
-function calculateResults(e){
+function calculateResults(){
   console.log('Calculating...');
   //UI vars
   const amount = document.getElementById('amount');
@@ -24,16 +33,27 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    //show results
+    document.getElementById('results').style.display = 'block';
+
+    //hide loader
+    document.getElementById('loading').style.display = 'none';
   }else {
     showError('Please check your numbers');
   }
 
 
-  e.preventDefault();
 }
 
 //Show Error
 function showError(error){
+  //show results
+  document.getElementById('results').style.display = 'none';
+
+  //hide loader
+  document.getElementById('loading').style.display = 'none';
+
   //Create a div
   const errorDiv = document.createElement('div');
 
@@ -58,3 +78,4 @@ function showError(error){
 function clearError(){
   document.querySelector('.alert').remove();
 }
+
